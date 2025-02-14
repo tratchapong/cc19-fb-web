5d5c6a0a94824381c9e16fade5c2cd95aaa9b644
tratchapong <tratchapong@gmail.com>
2025-02-14 13:30:16 +0700

    start with tailwind+daisyui

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl border bg-pink-300'>Codecamp Thailand</h1>
+      <button className="btn btn-circle btn-primary">OK</button>
+    </>
+  )
+}
+
+export default App
