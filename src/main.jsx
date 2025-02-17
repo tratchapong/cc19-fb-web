6f9bdd7da45c58a190f5efe6dce4279f851e49f2
tratchapong <tratchapong@gmail.com>
2025-02-17 11:39:37 +0700

    Register : clear input + validation & toast

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import AppRouter from './routes/AppRouter.jsx'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 createRoot(document.getElementById('root')).render(
   <>
     <AppRouter />
+    <ToastContainer 
+      position="bottom-center"
+    />
   </>
 
 )
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,34 +1,86 @@
-import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+
+const initInput = {
+	firstName: '',
+	lastName: '',
+	identity: '',
+	password: '',
+	confirmPassword: ''
+}
 
 function Register() {
+
+	const [input, setInput] = useState(initInput)
+
+	const hdlChange = e => {
+		setInput( prv => ({...prv, [e.target.name] : e.target.value}))
+	}
+
+	const hdlClearInput = () => {
+	 setInput(initInput)
+	}
+
+	const hdlRegister = e => {
+		const {firstName, lastName, identity, password, confirmPassword} = input
+		e.preventDefault()
+		//validation
+		if(!firstName.trim() || !lastName.trim() || !identity.trim() || !password.trim()) {
+			return toast.error('Please fill all inputs')
+		}
+		if(password !== confirmPassword) {
+			return toast('Password and Confirm password unmatched!!')
+		}
+		toast.success('ok', {position : 'top-center'})
+	}
+
 	return (
 		<>
 			<div className="text-3xl text-center opacity-70">Create a new account</div>
 			<div className="divider opacity-60"></div>
-			<form className='flex flex-col gap-5 p-4 pt-3'>
+			<form onSubmit={hdlRegister} className='flex flex-col gap-5 p-4 pt-3'>
 				<div className="flex gap-2">
 					<input type="text" 
 						placeholder='First name'
 						className='input input-bordered w-full'
+						name='firstName'
+						value={input.firstName}
+						onChange={hdlChange}
 					/>
 					<input type="text" 
 						placeholder='Last name'
 						className='input input-bordered w-full'
+						name='lastName'
+						value={input.lastName}
+						onChange={hdlChange}
 					/>
 				</div>
 				<input type="text" 
 						placeholder='Email or Phone number'
 						className='input input-bordered w-full'
+						name='identity'
+						value={input.identity}
+						onChange={hdlChange}
 				/>
 				<input type="password" 
 						placeholder='New password'
 						className='input input-bordered w-full'
+						name='password'
+						value={input.password}
+						onChange={hdlChange}
 				/>
 				<input type="password" 
 						placeholder='Confirm password'
 						className='input input-bordered w-full'
+						name='confirmPassword'
+						value={input.confirmPassword}
+						onChange={hdlChange}
 				/>
 				<button className='btn btn-secondary text-xl text-white'>Sign up</button>
+				<button className='btn btn-warning text-xl text-white'
+					type='button'
+					onClick={hdlClearInput}
+				>Reset</button>
 			</form>
 		</>
 	)
