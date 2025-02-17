3ba55cb82049318074e3f4d3a4ad777f4e15bdc9
tratchapong <tratchapong@gmail.com>
2025-02-17 10:18:57 +0700

    Register UI : ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FacebookTitle } from '../icons'
+import Register from './Register'
 
 function Login() {
 	return (
@@ -50,12 +51,10 @@ function Login() {
 			</div>
 			<dialog id="register-form" className="modal">
 				<div className="modal-box">
-					<form method="dialog">
-						{/* if there is a button in form, it will close the modal */}
-						<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-					</form>
-					<h3 className="font-bold text-lg">Hello!</h3>
-					<p className="py-4">Press ESC key or click on ✕ button to close</p>
+					<button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+						onClick={ ()=> document.getElementById('register-form').close()}
+					>✕</button>
+					<Register />		
 				</div>
 			</dialog>
 		</>
