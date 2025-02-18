07d489f9a37f0da86006a5e4e1f96b221d8087e7
tratchapong <tratchapong@gmail.com>
2025-02-18 16:39:43 +0700

    fix MenuItem hover, use isOpen for PostForm modal

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function MenuItem(props) {
 	const {icon : Icon, text, ...restProps} = props
 	return (
-		<button className='btn bg-opacity-0 border-none shadow-none justify-start gap-2 hover:opacity-20'>
+		<button className='btn bg-opacity-0 border-none shadow-none justify-start gap-2 hover:opacity-20 w-full'>
 			<Icon {...restProps} />
 			{text}
 		</button>
